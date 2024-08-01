fe987ab2292910b2d76d3b373706ca71ad14b140
Edoardo Ruiz <44981900+edoardrui0@users.noreply.github.com>
2024-07-31 20:12:36 -0400

    git init
    added initial TTT board

---
+++ board.rb
@@ -0,0 +1,16 @@
+class Board
+  def new_board
+    board_pos = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    <<~HEREDOC
+      #{board_pos[0][0]} | #{board_pos[0][1]} | #{board_pos[0][2]}
+      ---------
+      #{board_pos[1][0]} | #{board_pos[1][1]} | #{board_pos[1][2]}
+      ---------
+      #{board_pos[2][0]} | #{board_pos[2][1]} | #{board_pos[2][2]}
+    HEREDOC
+  end
+end
+
+test = Board.new
+puts test.new_board
---
+++ player.rb
@@ -0,0 +1,6 @@
+test_get = gets
+
+p test_get.chomp
+
+# need to get input from user and update the board if the user selected between numbers 1-9
+# if not, alert the player they selected something that isn't allowed
